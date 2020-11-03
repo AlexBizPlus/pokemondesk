27711dbcb9dc14347a1f53a2f840dd413cb6599b
AlexBizPlus <alexeynejnov@gmail.com>
2020-11-03 23:04:07 +0700

    husky

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,8 +3,6 @@ import cn from 'classnames';
 import s from './App.module.scss';
 
 const App = () => {
-  console.log(`q`);
-
   return <div className={cn(s.header, 'color')}>The Awsome App Component!</div>;
 };
 
