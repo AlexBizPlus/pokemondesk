2dd8b31454153fe34265979e3ccae2c411229147
AlexBizPlus <alexeynejnov@gmail.com>
2020-11-03 22:19:17 +0700

    Homework-3.1

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,6 +3,8 @@ import cn from 'classnames';
 import s from './App.module.scss';
 
 const App = () => {
+  console.log(`q`);
+
   return <div className={cn(s.header, 'color')}>The Awsome App Component!</div>;
 };
 
