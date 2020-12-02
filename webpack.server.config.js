bb035587c1d4d16e3b3704eca4edd69221fc53f5
AlexBizPlus <alexeynejnov@gmail.com>
2020-12-02 22:13:37 +0700

    Homework-9

---
+++ webpack.server.config.js
@@ -0,0 +1,66 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  target: 'node',
+  entry: path.resolve(__dirname, 'src/server/server.js'),
+  output: {
+    filename: 'server.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  externals: [nodeExternals()],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'css-loader',
+            options: {
+              modules: {
+                mode: 'local',
+                localIdentName: '[name]__[local]__[hash:base64:5]',
+                auto: /\.module\.\w+$/i,
+              },
+            },
+          },
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.[tj]sx?$/,
+        use: {
+          loader: 'ts-loader',
+        },
+      },
+      {
+        test: /\.svg$/,
+        use: ['@svgr/webpack', 'url-loader'],
+      },
+      {
+        test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
+        use: ['url-loader'],
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/',
+            },
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+  },
+  devtool: 'source-map',
+};
